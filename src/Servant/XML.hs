0c8ea938ddd48b25238b2898e2c4d96353715b18
Jaakko Rinta-Filppula <jaakko.rf@gmail.com>
2018-07-14 17:25:45 +0300

    Accept text/xml content type

--- src/Servant/XML.hs
+++ src/Servant/XML.hs
@@ -20,6 +20,7 @@ module Servant.XML where
 
 import           Data.ByteString.Builder (toLazyByteString)
 import           Data.ByteString.Lazy (toStrict)
+import qualified Data.List.NonEmpty as NE
 import qualified Network.HTTP.Media as M
 import           Servant.API
 import           Xmlbf
@@ -42,7 +43,8 @@ import           Xmlbf.Xeno
 data XML
 
 instance Accept XML where
-  contentType _ = "application" M.// "xml" M./: ("charset", "utf-8")
+  contentTypes _ = "application" M.// "xml" M./: ("charset", "utf-8") NE.:|
+                 [ "text" M.// "xml" M./: ("charset", "utf-8") ]
 
 instance ToXml a => MimeRender XML a where
   mimeRender _ = toLazyByteString . encode . toXml
